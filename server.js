d13956cdccd558ab0b546019bcd9f00ad678cd69
Thanayut Panipakmontri <thanayut@192.168.1.7>
2018-07-13 11:18:09 +0700

    Re-enable stock price API

--- server.js
+++ server.js
@@ -151,6 +151,48 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/price', function (req, res) {
+  if (req.query.q) {
+    res.json(getQuotesPrice(req.query.q));
+  } else {
+      res.send("<center><br><br><br><br>"+ 
+              "<h2><font color=red>Please add some quotes to the request</font></h2>"+
+              "<br><h3>Example: [root]/price?q=KBANK,PTT,TTW</h4></center>");
+  }
+});
+
+function getQuotesPrice(quotes) {
+  var stocks = String(quotes).toUpperCase().split(',');
+  var prices = [];
+  stocks.forEach(function(stock) {
+      prices.push({s:stock, p: getStockPrice(stock)});
+  });
+  return prices;
+}
+
+// get stock price from settrade.com
+const sourceURL = 'http://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol=';
+const { JSDOM } = jsdom;
+
+function getStockPrice(stock) {
+  try {
+      var price;
+      var res = request('GET', sourceURL+stock);
+      var dom = new JSDOM(res.body.toString('utf-8'));
+
+      // Try latest price first
+      price = String(dom.window.document.getElementsByTagName('h1')[1].innerHTML).trim();
+      if(price == '-'){
+          // Return latest close price if latest price is not available
+          price = String(dom.window.document.getElementsByTagName('td')[1].innerHTML).trim();
+      }
+      return Number(price);
+  } catch (ex) {
+      console.log(ex);
+      return -1;
+  }
+}
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
