238a461fc3ea5ffb7183d35a9db5ddc54e08723a
Thanayut <thanayut@vmi.asia>
2019-03-31 16:44:15 +0700

    Revert "security fix"
    This reverts commit dec0de255db3cc2eee4d328a5ebd28839705f417.

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP;
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
